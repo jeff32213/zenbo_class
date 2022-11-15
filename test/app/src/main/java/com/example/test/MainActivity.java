b7998dc45ae437c562beaad9b4695cb508ce4759
jeff32213 <jefftang32213@gmail.com>
2022-11-15 23:20:33 +0800

    1

---
+++ test/app/src/main/java/com/example/test/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.asus.robotframework.API.RobotCallback;
+import com.asus.robotframework.API.RobotCmdState;
+import com.asus.robotframework.API.RobotCommand;
+import com.asus.robotframework.API.RobotErrorCode;
+import com.asus.robotframework.API.RobotFace;
+
+import org.json.JSONObject;
+
+public class MainActivity extends RobotActivity {
+
+    public MainActivity(RobotCallback robotCallback, RobotCallback.Listen robotListenCallback) {
+        super(robotCallback, robotListenCallback);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        robotAPI.robot.setExpression(RobotFace.HAPPY,"I am happy");
+
+    }
+
+
+    public static RobotCallback robotCallback = new RobotCallback() {
+        @Override
+        public void onResult(int cmd, int serial, RobotErrorCode err_code, Bundle result) {
+            super.onResult(cmd, serial, err_code, result);
+
+            Log.d("RobotDevSample", "onResult:"
+                    + RobotCommand.getRobotCommand(cmd).name()
+                    + ", serial:" + serial + ", err_code:" + err_code
+                    + ", result:" + result.getString("RESULT"));
+        }
+
+        @Override
+        public void onStateChange(int cmd, int serial, RobotErrorCode err_code, RobotCmdState state) {
+            super.onStateChange(cmd, serial, err_code, state);
+        }
+
+        @Override
+        public void initComplete() {
+            super.initComplete();
+
+        }
+    };
+
+
+    public static RobotCallback.Listen robotListenCallback = new RobotCallback.Listen() {
+        @Override
+        public void onFinishRegister() {
+
+        }
+
+        @Override
+        public void onVoiceDetect(JSONObject jsonObject) {
+
+        }
+
+        @Override
+        public void onSpeakComplete(String s, String s1) {
+
+        }
+
+        @Override
+        public void onEventUserUtterance(JSONObject jsonObject) {
+
+        }
+
+        @Override
+        public void onResult(JSONObject jsonObject) {
+
+        }
+
+        @Override
+        public void onRetry(JSONObject jsonObject) {
+
+        }
+    };
+
+    public MainActivity() {
+        super(robotCallback, robotListenCallback);
+    }
+}
